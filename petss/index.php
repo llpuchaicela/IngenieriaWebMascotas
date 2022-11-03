4e0b07a294956ead04b8b642ff0443dbcc3b936a
Lady Puchaicela <73542300+llpuchaicela@users.noreply.github.com>
2022-11-03 12:58:46 -0500

    Add files via upload

---
+++ petss/index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/slider1.jpg">
+	</section>
+	<section class="titulo">
+		<h2>Adopción</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/dog1.jpg">
+		<section class="cardog">
+			<h3>Firulais</h3>
+			<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			<button>Mas info</button>
+		</section>
+	</section>
+	<section class="adopcion2">
+		<img src="images/dog2.jpg">
+		<section class="rabbit">
+			<h3>Rabbit</h3>
+			<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			<button>Mas info</button>
+		</section>
+	</section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
